7e43d59d6a96b3688645d9f65a777848970b4774
Nathan Lima <nathan.o.aguiar@gmail.com>
2025-07-10 18:48:28 -0300

    fix

--- lib/app/domains/dtos/people/people_filter.dart
+++ lib/app/domains/dtos/people/people_filter.dart
@@ -4,6 +4,7 @@ class PeopleFilter {
     this.aptidoes,
     this.idade,
     this.encontro,
+    this.hasTeam,
     this.circulo,
   });
 
@@ -12,16 +13,16 @@ class PeopleFilter {
   int? idade;
   int? encontro;
   String? circulo;
-
+  bool? hasTeam;
   Map<String, Object> toJson() {
     final year = idade != null ? DateTime.now().year - idade! : null;
     final json = <String, Object>{};
 
-    
     if (aptidoes != null) json.addAll({'aptidoes': aptidoes!});
     if (year != null) json.addAll({'aniversario': year});
     if (encontro != null) json.addAll({'ejc_fez': '$encontro'});
     if (circulo != null) json.addAll({'circulo': circulo!});
+
     return json;
   }
 }
